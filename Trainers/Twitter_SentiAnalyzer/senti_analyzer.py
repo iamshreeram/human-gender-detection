73c45ca1611c07338cd9e24c1ec01433212eb252
Shreeram <shreeram.v@live.com>
2017-05-18 13:27:19 -0600

    Formatting Structure

---
+++ Trainers/Twitter_SentiAnalyzer/senti_analyzer.py
@@ -0,0 +1,26 @@
+from textblob import TextBlob
+import tweepy
+
+cons_key = 's9uPtAY1McghyALKKQuxyKQlV'
+cons_sec = 'kSisXo1B1QAjrvxyeaobMi7q6f0jvzFDJtePMxEGeeSt1RNmBK'
+acces_tk = '222158291-12wkWBZnkZOK9ELjQHMzt724px4xm7blN8QUTXNt'
+acces_tk_sec = '5kyY86cL9FzTqBWL3ucbO3CgaZIppjQTcHNHIX5Jw8Xq3'
+
+auth = tweepy.OAuthHandler(cons_key,cons_sec)
+auth.set_access_token(acces_tk,acces_tk_sec)
+
+api = tweepy.API(auth)
+
+"""
+tweets = api.search('shreeram')
+
+for tweet in tweets:
+    print(tweet.text)
+    analysis = TextBlob(tweet.text)
+    print(analysis.sentiment)
+"""
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    analysis = TextBlob(tweet.text)
+    print analysis, analysis.sentiment
